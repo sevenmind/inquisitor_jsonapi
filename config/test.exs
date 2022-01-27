0003e74a1e5b5cab0c7e5ff1bb8881e7e3fd9403
Grant McLendon <grant@7mind.de>
2022-01-27 16:40:36 +0000

    replace mix.confg with config

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 config :inquisitor_jsonapi, ecto_repos: [Repo]
 config :inquisitor_jsonapi, Repo,
@@ -6,5 +6,6 @@ config :inquisitor_jsonapi, Repo,
   username: "postgres",
   password: "postgres",
   database: "inquisitor_jsonapi_test",
+  hostname: System.get_env("POSTGRES_HOST"),
   pool: Ecto.Adapters.SQL.Sandbox,
   size: 1
